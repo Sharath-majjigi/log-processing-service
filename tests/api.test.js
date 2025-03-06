63651c09dcff209402fee4767f2889845ca50ee9
sharath-majjigi <sharathholmes143@gmail.com>
2025-03-07 01:18:33 +0530

    feat: remove tests

--- tests/api.test.js
+++
@@ -1,9 +0,0 @@
-// tests/api.test.js
-import request from 'supertest';
-import app from '../src/api/index.js';
-
-test('POST /upload should return jobId', async () => {
-  const res = await request(app).post('/upload').attach('file', 'tests/test.log');
-  expect(res.status).toBe(200);
-  expect(res.body).toHaveProperty('jobId');
-});
